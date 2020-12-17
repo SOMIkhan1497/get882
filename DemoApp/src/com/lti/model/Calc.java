3d19734c5e2a86373c4b182b73ef3d62d0207d15
Seema <Seema@LAPTOP-1SM5FAE7>
2020-12-17 17:55:23 +0530

    added multiplication method in Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -7,5 +7,8 @@ public class Calc {
 	public void sub(int n1, int n2) {
 		System.out.println("Sub=" + (n1 - n2));
 	}
+	public void mul(int n1, int n2) {
+		System.out.println("mul=" + (n1 * n2));
+	}
 
 }
