b0b19f15eb81a10a0fee0d34f9328974a5ffd37b
Seemaila-khan <48855820+Seemaila-khan@users.noreply.github.com>
2020-12-18 13:05:43 +0530

    Update Calc.java
    added subs method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,8 +4,8 @@ public class Calc {
 	public void add(int n1, int n2) {
 		System.out.println("Sum=" + (n1 + n2));
 	}
-	public void sub(int n1, int n2) {
-		System.out.println("Sub=" + (n1 - n2));
+	public void subs(int n1, int n2) {
+		System.out.println("difference=" + (n1 - n2));
 	}
 
 }
