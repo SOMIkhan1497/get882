fcbeda1bed3098d85ccb924173c1f21f9abfca4c
Seemaila-khan <48855820+Seemaila-khan@users.noreply.github.com>
2020-12-18 15:41:29 +0530

    remove methods

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -1,13 +1,8 @@
 package com.lti.model;
 
 public class Calc {
-	public void add(int n1, int n2) {
-		System.out.println("Sum=" + (n1 + n2));
-	}
+	
 	public void subs(int n1, int n2) {
 		System.out.println("difference=" + (n1 - n2));
-	}
-	public void prod(int n1, int n2) {
-		System.out.println("prod=" + (n1 * n2));
 	}
 }
