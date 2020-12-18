53ea5cc44893c1292845dbe0a9d1c429477394a4
Seema <Seema@LAPTOP-1SM5FAE7>
2020-12-18 15:31:33 +0530

    removed methods

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,10 +4,4 @@ public class Calc {
 	public void add(int n1, int n2) {
 		System.out.println("Sum=" + (n1 + n2));
 	}
-	public void subs(int n1, int n2) {
-		System.out.println("difference=" + (n1 - n2));
-	}
-	public void prod(int n1, int n2) {
-		System.out.println("prod=" + (n1 * n2));
-	}
 }
